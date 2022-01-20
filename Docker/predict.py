48bbc3f60a7c1523a984a053b0cc87b17a4f1543
Andres Mahia <andresmahiamorado@gmail.com>
2022-01-20 14:26:25 +0100

    Changed project structure

---
+++ Docker/predict.py
@@ -0,0 +1,45 @@
+import platform; print(platform.platform())
+import sys; print("Python", sys.version)
+import numpy as np; print("NumPy", np.__version__)
+import scipy; print("SciPy", scipy.__version__)
+import os
+import pandas as pd
+import pickle
+import sklearn
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer, MissingIndicator
+from sklearn.compose import ColumnTransformer
+from joblib import load
+
+import warnings
+warnings.filterwarnings('ignore')
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+def predict():
+
+    dirpath = os.getcwd()
+    print("dirpath = ", dirpath, "\n")
+    output_path = os.path.join(dirpath,'output.xlsx')
+    print(output_path,"\n")
+
+    xtest = pd.read_parquet('xtest.parquet')
+    ytest = pd.read_csv('ytest.csv')
+
+    # Cargamos el modelo
+    clf = load('lightgbm.joblib')
+
+    print("Model score and classification:")
+    print(clf.predict(xtest))
+    print(clf.predict_proba(xtest)[:,1])
+
+    print(pd.DataFrame(clf.predict_proba(xtest)[:,1]))
+    pd.DataFrame(clf.predict_proba(xtest)[:,1]).to_excel(output_path)
+
+if __name__ == '__main__':
+    predict()
